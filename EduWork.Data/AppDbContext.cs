76fa249116355ff99bd9776697a501a095eac7e2
Tin <55455483+TinSisko-gameDev@users.noreply.github.com>
2024-06-30 16:33:53 +0200

    Added tables into DbContext file. Added connection string for MS SSMS.

--- EduWork.Data/AppDbContext.cs
+++ EduWork.Data/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EduWork.Data.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,8 +8,21 @@ using System.Threading.Tasks;
 
 namespace EduWork.Data
 {
-    internal class AppDbContext : DbContext
+    public class AppDbContext : DbContext
     {
+        public DbSet<AnnualLeave> AnnualLeaves { get; set; } = null!;
+        public DbSet<AnnualLeaveRecord> AnnualLeaveRecords { get; set; } = null!;
+        public DbSet<AppRole> AppRoles { get; set; } = null!;
+        public DbSet<NonWorkingDay> NonWorkingDays { get; set; } = null!;
+        public DbSet<Project> Projects { get; set; } = null!;
+        public DbSet<ProjectRole> ProjectRoles { get; set; } = null!;
+        public DbSet<ProjectTime> ProjectTimeRecords { get; set; } = null!;
+        public DbSet<SickLeaveRecord> SickLeaveRecords { get; set; } = null!;
+        public DbSet<User> Users { get; set; } = null!;
+        public DbSet<UserProjectRole> UserProjectRoles { get; set; } = null!;
+        public DbSet<WorkDay> WorkDays { get; set; } = null!;
+        public DbSet<WorkDayTime> WorkDayTimeRecords { get; set; } = null!;
+
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
 
--- EduWork.WebAPI/Program.cs
+++ EduWork.WebAPI/Program.cs
@@ -1,6 +1,9 @@
+using EduWork.Data;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Identity.Web;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +18,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options => {
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
